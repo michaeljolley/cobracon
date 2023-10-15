9dc7068d14497a7863eac9acd983d3fc340286c7
Michael Jolley <mike@sparcapp.io>
2023-10-15 18:01:53 -0500

    Fixing static index

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,18 @@ import { $user } from '../stores/userStore';
 import Welcome from './Welcome';
 import Me from './Me';
 
-export default function App(props: { code: string | null }) {
+export default function App() {
 	const user = useStore($user);
+
+	const search = window.location.search;
+	const params = new URLSearchParams(search);
+	let code = null;
+
+	if (params.size > 0 && params.get('code')) {
+		code = params.get('code');
+	}
 
 	if (user) return <Me />;
 
-	return <Welcome code={props.code} />;
+	return <Welcome code={code} />;
 }
